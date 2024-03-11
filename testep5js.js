397569789354542ac8c1e7793b20ba72fc8904b7
Yanaguisawa <162853213+Yanaguisawa@users.noreply.github.com>
2024-03-10 22:36:10 -0300

    Create testep5js.js

---
+++ testep5js.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+  background("black")
+}
+
+function draw() {
+  stroke("#FFEB3B");
+  fill("#9C27B0");
+  if(mouseIsPressed){
+    rect(mouseX,mouseY,10,20)}
+  
+}
